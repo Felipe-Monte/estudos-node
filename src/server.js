47534db749ffb587a16e1aac9c589d3b57474bc5
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-07-10 21:56:01 -0300

    create migrations

--- src/server.js
+++ src/server.js
@@ -1,29 +1,28 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 
 const express = require("express")
 
 const routes = require("./routes")
+migrationsRun()
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
 
-database()
-
 app.use((error, request, response, next) => {
 
   if (error instanceof AppError) {
-    return response.status(error.statusCode).json({ 
+    return response.status(error.statusCode).json({
       status: "error",
       message: error.message
     })
   }
 
   console.error(error)
-  
+
   return response.status(500).json({
     status: "error",
     message: "Internal server error"
