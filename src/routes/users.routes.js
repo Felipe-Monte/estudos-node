349ac4d5584eca8f849370284889c7625426b51b
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2023-08-11 22:42:23 -0300

    user authentication

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
 
 const UsersController = require("../controller/UsersController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+
 const usersRoutes = Router()
 
 const userController = new UsersController()
 
 usersRoutes.post("/", userController.create)
-usersRoutes.put("/:id", userController.update)
+usersRoutes.put("/", ensureAuthenticated, userController.update)
 
 module.exports = usersRoutes  
